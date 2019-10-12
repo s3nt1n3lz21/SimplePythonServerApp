df1a7f92359edbf76ae8cdcd2384e94e4134442e
Neil Smith <s3nt1n3lz18@gmail.com>
2019-10-12 15:55:11 +0100

    Added Python Script

---
+++ entrypoint.py
@@ -0,0 +1,16 @@
+import socket
+​
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 3306
+print('Listening for connections from host: ', socket.gethostbyname(socket.gethostname()))
+​
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        # Setup the port and get it ready for listening for connections
+        s.bind((HOST, PORT))
+        s.listen(1)
+        print('Waiting for incoming connections...')
+        conn, addr = s.accept()  # Wait for incoming connections
+        conn.sendall('Connected to the python server!'.encode('utf-8'))
+        print('Connected to: ', addr)
